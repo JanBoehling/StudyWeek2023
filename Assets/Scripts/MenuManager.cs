6942aaabd8f5eaa3a469820f01d25243688a48dc
InspectorRaupe <jonas.kluge03@gmail.com>
2023-09-25 13:43:40 +0200

    Put in visual cues; made winning screan has moving mouse now;

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void LoadScene(int sceneId) => SceneManager.LoadScene(sceneId);
     public void ExitGame() => Application.Quit();
     public void OpenLink(string link) => System.Diagnostics.Process.Start(new Uri(link).ToString());
